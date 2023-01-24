200fd2bddc692226848d5ca8b8a298c26183982d
Aparna-devi-chinnu-R <aparnadevi.rajavel@thoughtworks.com>
2023-01-24 18:29:52 +0530

    [Aparna] Added . Funtionality to fizzbuzz based on input

--- src/main/java/com/thoughtworks/practice/Fizzbuzz.java
+++ src/main/java/com/thoughtworks/practice/Fizzbuzz.java
@@ -1,4 +1,27 @@
 package com.thoughtworks.practice;
 
 public class Fizzbuzz {
+    public String isFizzOrBuzz(String input) {
+
+        if(!isNumeric(input)) return input;
+        long number = Long.parseLong(input);
+        if(number % 5 == 0  && number % 3 == 0){
+            return "fizzbuzz";
+        } else if(number % 5 == 0){
+            return "buzz";
+        }else if(number % 3 == 0){
+            return "fizz";
+        }
+        return "not fizzy or buzzy";
+    }
+
+    private boolean isNumeric(String input) {
+
+        try{
+            Long.parseLong(input);
+            return true;
+        }catch (NumberFormatException ex){
+            return false;
+        }
+    }
 }
--- src/test/java/com/thoughtworks/practice/FizzbuzzTest.java
+++ src/test/java/com/thoughtworks/practice/FizzbuzzTest.java
@@ -1,7 +1,43 @@
 package com.thoughtworks.practice;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzbuzzTest {
 
+    @Test
+    void shouldReturnBuzzIfNumberIsDivisibleByFive() {
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+        String output = fizzbuzz.isFizzOrBuzz("5");
+        assertEquals("buzz",output);
+    }
+
+    @Test
+    void shouldReturnFizzIfNumberIsDivisibleByThree() {
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+        String output = fizzbuzz.isFizzOrBuzz("9");
+        assertEquals("fizz",output);
+    }
+
+    @Test
+    void shouldReturnFizzBuzzIfNumberIsDivisibleByBothThreeAndFive() {
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+        String output = fizzbuzz.isFizzOrBuzz("15");
+        assertEquals("fizzbuzz",output);
+    }
+
+    @Test
+    void shouldReturnNotFizzyOrBuzzyWhenNumberIsNotDivisibleByEitherFiveOrThree() {
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+        String output = fizzbuzz.isFizzOrBuzz("8");
+        assertEquals("not fizzy or buzzy",output);
+    }
+
+    @Test
+    void shouldReturnInputIfInputIsNotAnNumber() {
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+        String output = fizzbuzz.isFizzOrBuzz("random");
+        assertEquals("random",output);
+    }
 }
