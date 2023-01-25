731d4fef15a8941b51aa0aa5342d40241b0e4e66
Aparna-devi-chinnu-R <aparnadevi.rajavel@thoughtworks.com>
2023-01-25 13:11:24 +0530

    [Aparna] Refactor . Moved the fizzbuzz identification to different method

--- src/main/java/com/thoughtworks/practice/Fizzbuzz.java
+++ src/main/java/com/thoughtworks/practice/Fizzbuzz.java
@@ -2,25 +2,27 @@ package com.thoughtworks.practice;
 
 public class Fizzbuzz {
     public String isFizzOrBuzz(String input) {
+        if (!isNumeric(input)) return input;
+        return fizzbuzz(input);
+    }
 
-        if(!isNumeric(input)) return input;
+    private String fizzbuzz(String input) {
         long number = Long.parseLong(input);
-        if(number % 5 == 0  && number % 3 == 0){
+        if (number % 5 == 0 && number % 3 == 0) {
             return "fizzbuzz";
-        } else if(number % 5 == 0){
+        } else if (number % 5 == 0) {
             return "buzz";
-        }else if(number % 3 == 0){
+        } else if (number % 3 == 0) {
             return "fizz";
         }
         return "not fizzy or buzzy";
     }
 
     private boolean isNumeric(String input) {
-
-        try{
+        try {
             Long.parseLong(input);
             return true;
-        }catch (NumberFormatException ex){
+        } catch (NumberFormatException ex) {
             return false;
         }
     }
